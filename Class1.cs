13866207cf90fbaabcf493001ac3ff5c8928da4e
evgenija12345 <100587239+evgenija12345@users.noreply.github.com>
2022-04-03 13:35:06 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(EvgenijaGjurovska3.Class1))]
+namespace EvgenijaGjurovska3
+{
+    public class Class1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
